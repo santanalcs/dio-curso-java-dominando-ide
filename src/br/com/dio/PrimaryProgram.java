0cf2bdb66df40dc4ef0d0b5386b5345d66e1b50a
Santana <santanalcs@hotmail.com>
2021-11-18 22:32:07 -0300

    First version

---
+++ src/br/com/dio/PrimaryProgram.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimaryProgram {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World");
+	
+		Gato gato = new Gato("Miau", "Branco", 5);
+		
+		System.out.println(gato.getName());
+	}
+
+}
---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,59 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+	private String name;
+	private String color;
+	private Integer age;
+
+	public Gato(String name, String color, int age) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, color, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gato other = (Gato) obj;
+		return Objects.equals(age, other.age) && Objects.equals(color, other.color) && Objects.equals(name, other.name);
+	}
+
+}
